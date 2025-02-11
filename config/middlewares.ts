7318ee175d58d261eda7a6e52417229b30dcee7c
geoff <g@vhslab.com>
2025-02-11 22:18:49 +1100

    should fix media view

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,18 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'script-src': ["'self'", "'unsafe-inline'", 'cdn.jsdelivr.net'],
+          'img-src': ["'self'", 'data:', 'cdn.jsdelivr.net', 'strapi.io', 'res.cloudinary.com', 'market-assets.strapi.io'],
+          'media-src': ["'self'", 'data:',  'res.cloudinary.com', 'market-assets.strapi.io'],
+        },
+      }
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
